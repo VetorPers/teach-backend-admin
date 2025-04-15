b9c336a5a4f9bf5c3186ef6aeaa09240af960f8a
肖韦 <506969463@qq.com>
2025-04-15 14:47:06 +0800

    deploy

--- database/migrations/2025_04_11_152804_create_orders_table.php
+++ database/migrations/2025_04_11_152804_create_orders_table.php
@@ -19,11 +19,14 @@ class CreateOrdersTable extends Migration
             $table->integer('student_id', false, true)->comment('学生id')->index();
             $table->integer('teacher_id', false, true)->comment('教师id')->index();
             $table->integer('course_id', false, true)->comment('课程id')->index();
+            $table->integer('sender_id', false, true)->comment('发送人id')->default(0)->index();
+            $table->dateTime('send_time')->nullable()->comment('发送时间');
             $table->decimal('amount', 10)->comment('订单金额');
             $table->tinyInteger('status', false, true)->default(0)->commnet('订单状态: 0,待发送 1,已发送');
             $table->tinyInteger('pay_status', false, true)->default(0)->commnet('支付状态: 0,未支付 1,已支付 2,支付失败');
             $table->dateTime('pay_time')->nullable()->comment('支付时间');
             $table->json('pay_info')->nullable()->comment('三方回调支付信息');
+            $table->string('pay_id')->nullable()->unique()->comment('三方订单号');
 
             $table->timestamps();
             $table->unique(['student_id', 'course_id']);
